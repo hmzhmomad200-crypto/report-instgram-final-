569252de94335d34fdc6fe25c26030eaaf88c864
hmzhmomad200-crypto <hmzhmomad200@gmail.com>
2026-05-28 16:38:03 +0300

    Add files via upload

---
+++ states/__init__.py
@@ -0,0 +1,1 @@
+from .states import SessionState, TargetState, BroadcastState
---
+++ states/states.py
@@ -0,0 +1,13 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class SessionState(StatesGroup):
+    waiting_for_session = State()
+
+class TargetState(StatesGroup):
+    waiting_for_username = State()
+
+class ReportState(StatesGroup):
+    selecting_types = State()
+
+class BroadcastState(StatesGroup):
+    waiting_for_message = State()
